c268d39110492965ccff6ef6429b1352c0b8cd96
viet.dinhduc <ducviet@gmail.com>
2022-08-09 23:02:49 +0700

    update bio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from 'react';
 
 function App() {
+  // const [counter, setcounter] = useState(1);
+  const [info, setInfo] = useState({
+    name: 'thanh hang tran',
+    age: 23,
+    address: 'Hanoi Vietnam'
+  })
+
+  function handleUpdateInfo() {
+    //cach 1
+    // setInfo({
+    //   ...info,
+    //   bio: 'love cofee and flower'
+    // });
+    //cach 2
+    setInfo(prev => ({
+      ...prev,
+      bio: 'love cofee and flower'
+    // });
+    }))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{JSON.stringify(info)}</h1>
+        <button onClick={handleUpdateInfo}>Update info</button>
       </header>
     </div>
   );
