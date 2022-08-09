bc2ad8738ea3a76c40450dab01d2c2a91bc140cb
viet.dinhduc <ducviet@gmail.com>
2022-08-09 23:18:57 +0700

    update random gifts

--- src/App.js
+++ src/App.js
@@ -2,33 +2,49 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from 'react';
 
+const gifts = [
+  'CPU i9',
+  'RAM 32GB',
+  'RGB Keyboard' 
+]
+
+
 function App() {
   // const [counter, setcounter] = useState(1);
-  const [info, setInfo] = useState({
-    name: 'thanh hang tran',
-    age: 23,
-    address: 'Hanoi Vietnam'
-  })
+  // const [info, setInfo] = useState({
+  //   name: 'thanh hang tran',
+  //   age: 23,
+  //   address: 'Hanoi Vietnam'
+  // })
 
-  function handleUpdateInfo() {
-    //cach 1
-    // setInfo({
-    //   ...info,
-    //   bio: 'love cofee and flower'
-    // });
-    //cach 2
-    setInfo(prev => ({
-      ...prev,
-      bio: 'love cofee and flower'
-    // });
-    }))
+  // function handleUpdateInfo() {
+  //   //cach 1
+  //   // setInfo({
+  //   //   ...info,
+  //   //   bio: 'love cofee and flower'
+  //   // });
+  //   //cach 2
+  //   setInfo(prev => ({
+  //     ...prev,
+  //     bio: 'love cofee and flower'
+  //   // });
+  //   }))
+  // }
+
+  const [gift, setGift] = useState()
+
+  const randomGift = () => {
+    const index = Math.floor(Math.random()*gifts.length)
+    setGift(gifts[index]);
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{JSON.stringify(info)}</h1>
-        <button onClick={handleUpdateInfo}>Update info</button>
+        {/* <h1>{JSON.stringify(info)}</h1>
+        <button onClick={handleUpdateInfo}>Update info</button> */}
+        <h1>{gift || 'you have no gift'}</h1>
+        <button onClick = {randomGift}>Get gift</button>
       </header>
     </div>
   );
