bd223d247cf013ef95b28112209f35751ea15b8e
viet.dinhduc <ducviet@gmail.com>
2022-08-09 23:54:34 +0700

    handle checkbox

--- src/App.js
+++ src/App.js
@@ -1,51 +1,38 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const gifts = [
-  'CPU i9',
-  'RAM 32GB',
-  'RGB Keyboard' 
-]
-
+const courses = [
+  {
+    id: 1,
+    name: "HTML, CSS",
+  },
+  {
+    id: 2,
+    name: "JavaScript",
+  },
+  {
+    id: 3,
+    name: "ReactJS",
+  },
+];
 
 function App() {
-  // const [counter, setcounter] = useState(1);
-  // const [info, setInfo] = useState({
-  //   name: 'thanh hang tran',
-  //   age: 23,
-  //   address: 'Hanoi Vietnam'
-  // })
+  const [checked, setChecked] = useState();
 
-  // function handleUpdateInfo() {
-  //   //cach 1
-  //   // setInfo({
-  //   //   ...info,
-  //   //   bio: 'love cofee and flower'
-  //   // });
-  //   //cach 2
-  //   setInfo(prev => ({
-  //     ...prev,
-  //     bio: 'love cofee and flower'
-  //   // });
-  //   }))
-  // }
+  console.log(checked);
 
-  const [gift, setGift] = useState()
-
-  const randomGift = () => {
-    const index = Math.floor(Math.random()*gifts.length)
-    setGift(gifts[index]);
-  }
+  const handleSubmit = () => {};
 
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <h1>{JSON.stringify(info)}</h1>
-        <button onClick={handleUpdateInfo}>Update info</button> */}
-        <h1>{gift || 'you have no gift'}</h1>
-        <button onClick = {randomGift}>Get gift</button>
-      </header>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <input type="radio" checked={checked === course.id} onChange={() => setChecked(course.id)} />
+          {course.name}
+        </div>
+      ))}
+      <button onClick={handleSubmit}>Register</button>
     </div>
   );
 }
